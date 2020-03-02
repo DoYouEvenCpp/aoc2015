15f14ab89f341018f07aa1efac571bf3b77278de
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-02 15:53:21 +0100

    Day09 pretty much WIP

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,8 +1,13 @@
 use std::fs;
+use itertools::Itertools;
 
-type DistanceTo = (String, usize);
-type Entry = (String, DistanceTo);
-type MapType = std::vec::Vec<Entry>;
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+struct Edge {
+    vertex: String,
+    distance: usize
+}
+
+type MapType = std::collections::HashMap<String, Vec<Edge>>;
 
 fn handle_entry(line: &str, map: &mut MapType) {
     let splitted: Vec<&str> = line.splitn(2, " = ").collect();
@@ -11,8 +16,11 @@ fn handle_entry(line: &str, map: &mut MapType) {
         .map(|s| s.to_owned())
         .collect();
     //println!("{} to {} in {}", towns[0], towns[1], distance);
-    map.push((towns[0].to_owned(), (towns[1].to_owned(), distance)));
-    map.push((towns[1].to_owned(), (towns[0].to_owned(), distance)));
+    let e = map.entry(towns[0].to_owned()).or_default();
+    (*e).push(Edge{vertex: towns[1].to_owned(), distance});
+
+    let e = map.entry(towns[1].to_owned()).or_default();
+    (*e).push(Edge{vertex: towns[0].to_owned(), distance});
 }
 
 fn parse_input(input: &str) -> MapType {
@@ -36,8 +44,49 @@ fn main() {
     let content = content.trim();
     let map = parse_input(content);
 
-    println!("First puzzle: {}", part_1(&content));
+    for (town, edges) in &map {
+        print!("{}: ", town);
+        for e in edges {
+            print!("{}({}), ", e.vertex, e.distance)
+        }
+        println!();
+    }
+
+    println!("\nPermutations\n");
+    let mut distances = std::collections::HashSet::new();
+    for (start, vertexes) in map {
+        for perm in vertexes.iter().permutations(vertexes.len()).unique() {
+            //println!("{:?}\n\n", perm[0]);
+            //  for (edge, vertexes) in perm {
+                    let sum = perm.iter().fold(0, |acc, edge| acc + edge.distance);
+                    distances.insert(sum);
+            //      print!("{} -> ", edge);
+            //      for edge in vertexes {
+            //          print!("{}({}), ", edge.vertex, edge.distance);
+            //      }
+            //      println!();
+            //  }
+            //println!("{:?}", perm);
+        }
+    }
+    // for perm in map.iter().permutations(map.len()).unique() {
+    //     //println!("{:?}\n\n", perm[0]);
+    //      for (edge, vertexes) in perm {
+    //          let sum = vertexes.iter().fold(0, |acc, edge| acc + edge.distance);
+    //          distances.insert(sum);
+    //          print!("{} -> ", edge);
+    //          for edge in vertexes {
+    //              print!("{}({}), ", edge.vertex, edge.distance);
+    //          }
+    //          println!();
+    //      }
+    // }
+
+
+    println!("First puzzle: {:?}", distances);
     println!("Second puzzle: {}", part_2(&content));
+
+    //333 NOK!
 }
 
 #[cfg(test)]
