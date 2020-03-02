c4bf473209e07f590d387ebe168d8db19995d598
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-02 13:50:32 +0100

    Day09
    Input parsing implemented

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,5 +1,27 @@
 use std::fs;
 
+type DistanceTo = (String, usize);
+type Entry = (String, DistanceTo);
+type MapType = std::vec::Vec<Entry>;
+
+fn handle_entry(line: &str, map: &mut MapType) {
+    let splitted: Vec<&str> = line.splitn(2, " = ").collect();
+    let distance: usize = splitted[1].to_string().parse().unwrap();
+    let towns: Vec<String> = splitted[0].splitn(2, " to ")
+        .map(|s| s.to_owned())
+        .collect();
+    //println!("{} to {} in {}", towns[0], towns[1], distance);
+    map.push((towns[0].to_owned(), (towns[1].to_owned(), distance)));
+    map.push((towns[1].to_owned(), (towns[0].to_owned(), distance)));
+}
+
+fn parse_input(input: &str) -> MapType {
+    let mut map = MapType::new();
+    for line in input.lines() {
+        handle_entry(line, &mut map);
+    }
+    map
+}
 
 fn part_1(input: &str) -> usize {
     0
@@ -12,6 +34,12 @@ fn part_2(input: &str) -> usize {
 fn main() {
     let content = fs::read_to_string("input").expect("file not found");
     let content = content.trim();
+    let map = parse_input(content);
+
+    for (town, distance) in map {
+        println!("{} -> {} {}", town, distance.0, distance.1);
+    }
+    let mut perms = map[..].permutations();
     println!("First puzzle: {}", part_1(&content));
     println!("Second puzzle: {}", part_2(&content));
 }
