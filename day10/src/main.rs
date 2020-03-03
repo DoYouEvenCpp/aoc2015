aca35c8fb0d341d671d3fb1fed2a22c84e58841d
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-03 15:55:04 +0100

    implemenation almost ready

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,10 +1,19 @@
 use std::fs;
 
 fn single_iteration(input: &str) -> String {
-    let numbers: Vec<(u32, u32)>;
+    let digits: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let mut numbers: Vec<(u32, u32)> = vec![];
     let res: String = "".to_owned();
-    for ch in input.chars() {
+    for ch in digits {
+        if let Some(e) = numbers.last_mut() {
+            if e.0 == ch {
+                e.1 += 1;
+            }
+        } else {
+            numbers.push((ch, 1));
+        }
     }
+    println!("{:?}", numbers);
     res
 }
 
@@ -18,6 +27,7 @@ fn part_2(input: &str) -> usize {
 
 fn main() {
     let content = "1321131112";
+    single_iteration("11");
     println!("First puzzle: {}", part_1(&content));
     println!("Second puzzle: {}", part_2(&content));
 }
