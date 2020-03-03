0988960bfb19c0737ebbb54f2e456d10daa4a408
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-03 15:44:38 +0100

    Day10 initial commit

---
+++ day10/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+fn single_iteration(input: &str) -> str {
+    let numbers: Vec<(u32, u32)>;
+    for ch in input.chars() {
+    }
+}
+
+fn part_1(input: &str) -> usize {
+    0
+}
+
+fn part_2(input: &str) -> usize {
+    0
+}
+
+fn main() {
+    let content = "1321131112";
+    println!("First puzzle: {}", part_1(&content));
+    println!("Second puzzle: {}", part_2(&content));
+}
+
+#[cfg(test)]
+mod day08 {
+    use super::*;
+}
