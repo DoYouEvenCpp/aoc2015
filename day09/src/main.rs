bb14c7eab9c1de7a2dc1b9f5aa53d3527529bd03
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-03 15:22:19 +0100

    Day09 solved
    quick fox on searchin algorithm

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,10 +1,10 @@
-use std::fs;
 use itertools::Itertools;
+use std::fs;
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
 struct Edge {
     vertex: String,
-    distance: usize
+    distance: usize,
 }
 
 type MapType = std::collections::HashMap<String, Vec<Edge>>;
@@ -12,15 +12,21 @@ type MapType = std::collections::HashMap<String, Vec<Edge>>;
 fn handle_entry(line: &str, map: &mut MapType) {
     let splitted: Vec<&str> = line.splitn(2, " = ").collect();
     let distance: usize = splitted[1].to_string().parse().unwrap();
-    let towns: Vec<String> = splitted[0].splitn(2, " to ")
+    let towns: Vec<String> = splitted[0]
+        .splitn(2, " to ")
         .map(|s| s.to_owned())
         .collect();
-    //println!("{} to {} in {}", towns[0], towns[1], distance);
     let e = map.entry(towns[0].to_owned()).or_default();
-    (*e).push(Edge{vertex: towns[1].to_owned(), distance});
+    (*e).push(Edge {
+        vertex: towns[1].to_owned(),
+        distance,
+    });
 
     let e = map.entry(towns[1].to_owned()).or_default();
-    (*e).push(Edge{vertex: towns[0].to_owned(), distance});
+    (*e).push(Edge {
+        vertex: towns[0].to_owned(),
+        distance,
+    });
 }
 
 fn parse_input(input: &str) -> MapType {
@@ -31,65 +37,31 @@ fn parse_input(input: &str) -> MapType {
     map
 }
 
-fn part_1(input: &str) -> usize {
-    0
-}
-
-fn part_2(input: &str) -> usize {
-    0
-}
-
 fn main() {
     let content = fs::read_to_string("input").expect("file not found");
     let content = content.trim();
     let map = parse_input(content);
 
-    for (town, edges) in &map {
-        print!("{}: ", town);
-        for e in edges {
-            print!("{}({}), ", e.vertex, e.distance)
-        }
-        println!();
-    }
-
-    println!("\nPermutations\n");
+    //for sure that is UGLY
     let mut distances = std::collections::HashSet::new();
-    for (start, vertexes) in map {
+    let mut current_town: String;
+    for (start, vertexes) in &map {
         for perm in vertexes.iter().permutations(vertexes.len()).unique() {
-            //println!("{:?}\n\n", perm[0]);
-            //  for (edge, vertexes) in perm {
-                    let sum = perm.iter().fold(0, |acc, edge| acc + edge.distance);
-                    distances.insert(sum);
-            //      print!("{} -> ", edge);
-            //      for edge in vertexes {
-            //          print!("{}({}), ", edge.vertex, edge.distance);
-            //      }
-            //      println!();
-            //  }
-            //println!("{:?}", perm);
+            current_town = start.to_owned();
+            let mut sum = 0;
+            for edge in perm {
+                sum += map
+                    .get(&current_town)
+                    .unwrap()
+                    .iter()
+                    .filter(|e| e.vertex == edge.vertex)
+                    .fold(0, |acc, e| acc + e.distance);
+                current_town = edge.vertex.to_owned();
+            }
+            distances.insert(sum);
         }
     }
-    // for perm in map.iter().permutations(map.len()).unique() {
-    //     //println!("{:?}\n\n", perm[0]);
-    //      for (edge, vertexes) in perm {
-    //          let sum = vertexes.iter().fold(0, |acc, edge| acc + edge.distance);
-    //          distances.insert(sum);
-    //          print!("{} -> ", edge);
-    //          for edge in vertexes {
-    //              print!("{}({}), ", edge.vertex, edge.distance);
-    //          }
-    //          println!();
-    //      }
-    // }
 
-
-    println!("First puzzle: {:?}", distances);
-    println!("Second puzzle: {}", part_2(&content));
-
-    //333 NOK!
-}
-
-#[cfg(test)]
-mod day09 {
-    use super::*;
+    println!("First puzzle: {}", distances.iter().min().unwrap());
+    println!("Second puzzle: {}", distances.iter().max().unwrap());
 }
