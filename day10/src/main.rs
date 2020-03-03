6fff3fcb52f2b92a5769bdaad6541103a36cbed4
Stanislaw Kubiak <stahok@gmail.com>
2020-03-03 19:43:27 +0100

    Day10 solved

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,38 +1,58 @@
-use std::fs;
 
-fn single_iteration(input: &str) -> String {
-    let digits: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+fn single_iteration(input: &Vec<u32>) -> Vec<u32> {
     let mut numbers: Vec<(u32, u32)> = vec![];
-    let res: String = "".to_owned();
-    for ch in digits {
-        if let Some(e) = numbers.last_mut() {
-            if e.0 == ch {
-                e.1 += 1;
-            }
+    for ch in input {
+        if numbers.len() == 0 {
+            numbers.push((*ch, 1));
+        }
+        else if numbers.last().unwrap().0 == *ch {
+            numbers.last_mut().unwrap().1 += 1;
         } else {
-            numbers.push((ch, 1));
+            numbers.push((*ch, 1));
         }
     }
-    println!("{:?}", numbers);
+    let mut res: Vec<u32> = vec![];
+    for (ch, count) in &numbers {
+        res.push(*count);
+        res.push(*ch);
+    }
     res
 }
 
 fn part_1(input: &str) -> usize {
-    0
+    let digits = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let mut res = digits;
+    for _ in 0..40 {
+        res = single_iteration(&res);
+    }
+    res.len()
 }
 
 fn part_2(input: &str) -> usize {
-    0
+    let digits = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let mut res = digits;
+    for _ in 0..50 {
+        res = single_iteration(&res);
+    }
+    res.len()
 }
 
 fn main() {
     let content = "1321131112";
-    single_iteration("11");
-    println!("First puzzle: {}", part_1(&content));
-    println!("Second puzzle: {}", part_2(&content));
+    println!("First puzzle: {:?}", part_1(&content));
+    println!("Second puzzle: {:?}", part_2(&content));
 }
 
 #[cfg(test)]
 mod day10 {
     use super::*;
+    #[test]
+    fn test_single_iteration(){
+        
+        assert_eq!([1,1], single_iteration(&"1".chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<u32>>()).as_slice());
+        assert_eq!([2,1], single_iteration(&"11".chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<u32>>()).as_slice());
+        assert_eq!([1,2,1,1], single_iteration(&"21".chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<u32>>()).as_slice());
+        assert_eq!([1,1,1,2,2,1], single_iteration(&"1211".chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<u32>>()).as_slice());
+        assert_eq!([3,1,2,2,1,1], single_iteration(&"111221".chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<u32>>()).as_slice());
+    }
 }
