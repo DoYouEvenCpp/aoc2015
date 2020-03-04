077b13001019d2619f6bbf44d9f6fc94b458a82b
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-04 11:59:54 +0100

    Day11 initiali implementation

---
+++ day11/src/main.rs
@@ -0,0 +1,88 @@
+fn contains_forbidden_letters(input: &str) -> bool {
+    let vowels = ['i', 'u', 'l'];
+    input
+        .chars()
+        .fold(0, |count, c| count + vowels.contains(&c) as u32)
+        > 0
+}
+
+fn contains_increasing_three_letters(input: &str) -> bool {
+    input
+        .as_bytes()
+        .windows(3)
+        .any(|chars| chars[1] - chars[0] == 1 && chars[2] - chars[1] == 1)
+}
+
+fn has_pair_repetition(input: &str) -> bool {
+    input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(2)
+        .enumerate()
+        .any(|(pos, first)| {
+            input
+                .chars()
+                .skip(pos + 2)
+                .collect::<Vec<char>>()
+                .windows(2)
+                .any(|second| {
+                    first[0] == first[1] && second[0] == second[1] && first[0] != second[0]
+                })
+        })
+}
+fn validate_password(input: &str) -> bool {
+    !contains_forbidden_letters(input)
+        && contains_increasing_three_letters(input)
+        && has_pair_repetition(input)
+}
+
+fn generate_next_password(input: &str) -> &str {
+    validate_password(input);
+    ""
+}
+
+fn part_1(input: &str) -> &str {
+    generate_next_password(input)
+}
+
+fn part_2(input: &str) -> usize {
+    0
+}
+
+fn main() {
+    let content = "hepxcrrq";
+    println!("First puzzle: {}", part_1(&content));
+    println!("Second puzzle: {}", part_2(&content));
+}
+
+#[cfg(test)]
+mod day11 {
+    use super::*;
+    #[test]
+    fn test_contains_increasing_three_letters() {
+        assert_eq!(true, contains_increasing_three_letters("hijklmmn"));
+        assert_eq!(false, contains_increasing_three_letters("abbceffg"));
+        assert_eq!(false, contains_increasing_three_letters("abbcegjk"));
+        assert_eq!(false, contains_increasing_three_letters(""));
+    }
+
+    #[test]
+    fn test_contains_forbidden_letters() {
+        assert_eq!(true, contains_forbidden_letters("hijklmmn"));
+        assert_eq!(false, contains_forbidden_letters("abbceffg"));
+        assert_eq!(false, contains_forbidden_letters("abbcegjk"));
+        assert_eq!(true, contains_forbidden_letters("iol"));
+        assert_eq!(false, contains_forbidden_letters(""));
+    }
+    #[test]
+    fn test_has_pair_repetition() {
+        assert_eq!(true, has_pair_repetition("abbceffg"));
+        assert_eq!(false, has_pair_repetition("abbcegjk"));
+        assert_eq!(false, has_pair_repetition("aaa"));
+    }
+    #[test]
+    fn test_validate_password() {
+        assert_eq!(true, validate_password("abcdffaa"));
+        assert_eq!(false, validate_password("aabbcciol"));
+    }
+}
