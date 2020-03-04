a655568103728b2fcc60ad9bb23d7830dca71566
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-04 15:47:47 +0100

    Day12, first puzzle solved

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,19 +1,29 @@
 use std::fs;
 use serde_json::{Result, Value};
 
-fn part_1(input: &Result) -> usize {
-    0
+fn part_1(i: &Value) -> i64 {
+    let mut sum = 0;
+    match i {
+        Value::Number(number) => sum += number.as_i64().unwrap(),
+        Value::Array(array) => sum += array.iter().map(|value| part_1(value)).sum::<i64>(),
+        Value::Object(object) => sum += object.values().map(|value| part_1(value)).sum::<i64>(),
+        _ => (),
+    };
+    sum
 }
 
-fn part_2(input: &Result) -> usize {
+fn part_2(input: &Value) -> i32 {
    0
 }
 
-fn main() {
+fn main() -> Result<()>{
     let content = fs::read_to_string("input").expect("file not found");
     let content = content.trim();
-    println!("First puzzle: {}", part_1(&content));
-    println!("Second puzzle: {}", part_2(&content));
+    let v: Value = serde_json::from_str(content)?;
+
+    println!("First puzzle: {}", part_1(&v));
+    println!("Second puzzle: {}", part_2(&v));
+    Ok(())
 }
 
 #[cfg(test)]
