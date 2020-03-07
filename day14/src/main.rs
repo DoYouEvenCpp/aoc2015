22e432dde8a9fcc6c053902710463d8430386ce8
Stanislaw Kubiak <stahok@gmail.com>
2020-03-07 14:21:37 +0100

    Day14 solved Both puzzles

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -3,25 +3,25 @@ use std::collections::HashMap;
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
 struct Velocity {
-    speed: u32,
+    velocity: u32,
     time: u32,
     rest: u32,
 }
 
 type DataType = std::collections::HashMap<String, Velocity>;
-type Leaderboard = std::collections::HashMap<String, u32>;
+type Leaderboard = std::collections::HashMap<String, (u32, u32)>;
 
 fn parse_input() -> DataType {
     let mut m = DataType::new();
-    m.insert("Vixen".to_string(),   Velocity{speed: 19, time: 7,    rest: 124});
-    m.insert("Rudolph".to_string(), Velocity{speed: 3,  time: 15,   rest: 28});
-    m.insert("Donner".to_string(),  Velocity{speed: 19, time: 9,    rest: 164});
-    m.insert("Blitzen".to_string(), Velocity{speed: 19, time: 9,    rest: 158});
-    m.insert("Comet".to_string(),   Velocity{speed: 13, time: 7,    rest: 82});
-    m.insert("Cupid".to_string(),   Velocity{speed: 25, time: 6,    rest: 145});
-    m.insert("Dasher".to_string(),  Velocity{speed: 14, time: 3,    rest: 38});
-    m.insert("Dancer".to_string(),  Velocity{speed: 3,  time: 16,   rest: 37});
-    m.insert("Prancer".to_string(), Velocity{speed: 25, time: 6,    rest: 143});
+    m.insert("Vixen".to_string(),   Velocity{velocity: 19, time: 7,    rest: 124});
+    m.insert("Rudolph".to_string(), Velocity{velocity: 3,  time: 15,   rest: 28});
+    m.insert("Donner".to_string(),  Velocity{velocity: 19, time: 9,    rest: 164});
+    m.insert("Blitzen".to_string(), Velocity{velocity: 19, time: 9,    rest: 158});
+    m.insert("Comet".to_string(),   Velocity{velocity: 13, time: 7,    rest: 82});
+    m.insert("Cupid".to_string(),   Velocity{velocity: 25, time: 6,    rest: 145});
+    m.insert("Dasher".to_string(),  Velocity{velocity: 14, time: 3,    rest: 38});
+    m.insert("Dancer".to_string(),  Velocity{velocity: 3,  time: 16,   rest: 37});
+    m.insert("Prancer".to_string(), Velocity{velocity: 25, time: 6,    rest: 143});
     m
 }
 
@@ -34,7 +34,7 @@ fn part_1(input: &DataType, time: u32) -> u32 {
         if modulo_rest >= val.time {
             count += 1;
         }
-        let s = val.speed * count * val.time;
+        let s = val.velocity * count * val.time;
         distances.insert(s);
     }
     *distances.iter().max().unwrap()
@@ -42,40 +42,35 @@ fn part_1(input: &DataType, time: u32) -> u32 {
 
 fn part_2(input: &mut DataType, t: u32) -> u32 {
     let mut current_leaderboard = Leaderboard::new();
-    current_leaderboard.insert("Vixen".to_string(), 0);
-    current_leaderboard.insert("Rudolph".to_string(), 0);
-    current_leaderboard.insert("Donner".to_string(), 0);
-    current_leaderboard.insert("Blitzen".to_string(), 0);
-    current_leaderboard.insert("Comet".to_string(), 0);
-    current_leaderboard.insert("Cupid".to_string(), 0);
-    current_leaderboard.insert("Dasher".to_string(), 0);
-    current_leaderboard.insert("Dancer".to_string(), 0);
-    current_leaderboard.insert("Prancer".to_string(), 0);
+    current_leaderboard.insert("Vixen".to_string(), (0,0));
+    current_leaderboard.insert("Rudolph".to_string(), (0,0));
+    current_leaderboard.insert("Donner".to_string(), (0,0));
+    current_leaderboard.insert("Blitzen".to_string(), (0,0));
+    current_leaderboard.insert("Comet".to_string(), (0,0));
+    current_leaderboard.insert("Cupid".to_string(), (0,0));
+    current_leaderboard.insert("Dasher".to_string(), (0,0));
+    current_leaderboard.insert("Dancer".to_string(), (0,0));
+    current_leaderboard.insert("Prancer".to_string(), (0,0));
 
-    for current_time in 1..=t {
+    for current_time in 0..=t {
         input.iter().for_each(|entry| {
             let the_rest = current_time % (entry.1.time + entry.1.rest);
-            if the_rest <= entry.1.time {
+            if the_rest < entry.1.time {
                 //still bursting
                 match current_leaderboard.get_mut(entry.0) {
-                    Some(v) => *v += entry.1.speed,
+                    Some(v) => v.0 += entry.1.velocity,
                     _ => panic!("xd")
                 }
             }
         });
-        let current_max = *current_leaderboard.iter().map(|(_, v)| v).max().unwrap();
-        //println!("{} {:?} {}",current_time, current_leaderboard, current_max);
-        //current_leaderboard.iter_mut().filter(|p| p.1 == &mut current_max).map(|p| *p.1 += 1);
+        let current_max = current_leaderboard.iter().map(|(_, v)| v).max().unwrap().0;
         for (_, val) in &mut current_leaderboard {
-            if *val == current_max {
-                *val += 1;
+            if val.0 == current_max {
+                val.1 += 1;
             }
         }
-        println!("{} {:?} {}",current_time, current_leaderboard, current_max);
     }
-    //817 - too low
-    //3277 - too high
-    0
+    current_leaderboard.iter().map(|(_, v)| v.1).max().unwrap()
 }
 
 fn main() {
@@ -93,7 +88,7 @@ mod day14 {
         data.insert(
             "Comet".to_string(),
             Velocity {
-                speed: 14,
+                velocity: 14,
                 time: 10,
                 rest: 127,
             },
@@ -101,7 +96,7 @@ mod day14 {
         data.insert(
             "Dancer".to_string(),
             Velocity {
-                speed: 16,
+                velocity: 16,
                 time: 11,
                 rest: 162,
             },
