dee1d4a6a47d3b4bcc719309b19f45ef3a38b5f5
Stanislaw Kubiak <stahok@gmail.com>
2020-03-10 23:24:21 +0100

    Day18 first puzzle solved

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -3,8 +3,6 @@ use std::fs;
 type Map = Vec<Vec<char>>;
 
 fn pase_input(input: &str) -> Map {
-    let mut x: usize = 0;
-    let mut y: usize = 0;
     let mut data = Map::new();
     for line in input.lines() {
         data.push(vec![]);
@@ -16,20 +14,29 @@ fn pase_input(input: &str) -> Map {
     data
 }
 
+// fn print_map(map: &Map) {
+//     for x in 0..100 {
+//         for y in 0..100 {
+//             print!("{} ", map[x][y]);
+//         }
+//         println!();
+//     }
+// }
+
 fn does_exist(x: i32, y: i32) -> bool {
-    x > 0 && x < 100 && y > 0 && y < 100
+    x >= 0 && x < 100 && y >= 0 && y < 100
 }
 
-fn get_number_of_neighbours(state: char, x: usize, y: usize, map: &Map) -> u32 {
+fn get_number_of_neighbours(x: usize, y: usize, map: &Map) -> u32 {
     [
         (-1, -1),
+        (-1, 0),
+        (-1, 1),
         (0, -1),
+        (0, 1),
         (1, -1),
         (1, 0),
-        (1, 1),
-        (0, 1),
-        (-1, 1),
-        (-1, 0)
+        (1, 1)
     ].iter()
     .filter(|offset| does_exist(x as i32 + offset.0, y as i32 + offset.1))
     .filter(|offset| map[(x as i32 + offset.0) as usize][(y as i32 + offset.1) as usize] == '#')
@@ -39,12 +46,12 @@ fn get_number_of_neighbours(state: char, x: usize, y: usize, map: &Map) -> u32 {
 
 fn part_1(input: &Map) -> u32 {
     let mut count: u32 = 0;
-    let mut input = input;
-    let mut tmp = input;
-    for iteration in 0..100 {
+    let mut input = input.to_owned();
+    let mut tmp = input.clone();
+    for _ in 0..100 {
         for x in 0..100 {
             for y in 0..100 {
-                let number_of_neighbours = get_number_of_neighbours(input[x][y], x, y, &input);
+                let number_of_neighbours = get_number_of_neighbours(x, y, &input);
                 match input[x][y] {
                     '#' => {
                         if number_of_neighbours < 2 || number_of_neighbours > 3 {
@@ -60,7 +67,7 @@ fn part_1(input: &Map) -> u32 {
                 }
             }
         }
-        input = tmp;
+        input = tmp.clone();
     }
     for x in 0..100 {
         for y in 0..100 {
@@ -78,8 +85,8 @@ fn part_2(input: &Map) -> u32 {
 
 fn main() {
     let content = fs::read_to_string("input").expect("file not found");
-    let mut content = pase_input(content.trim());
-    println!("First puzzle: {}", part_1(&mut content));
+    let content = pase_input(content.trim());
+    println!("First puzzle: {}", part_1(&content));
     println!("Second puzzle: {}", part_2(&content));
 }
 
