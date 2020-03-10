3a0504812c67e8f8cc676b35d94f982619399c13
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-10 15:22:59 +0100

    Day18
    added input reading

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,19 +1,25 @@
 use std::fs;
 
-type Map = Vec<Vec<u32>>;
-type Position = (usize, usize, usize, usize);
+type Map = Vec<Vec<char>>;
 
 fn pase_input(input: &str) -> Map {
     let mut x: usize = 0;
-    let mut x: usize = 0;
+    let mut y: usize = 0;
     let mut data = Map::new();
     for line in input.lines() {
+        data.push(vec![]);
         for c in line.chars() {
-
+            data.last_mut().unwrap().push(c);
         }
     }
+    data.truncate(input.lines().count());
     data
 }
+
+fn get_number_of_neighbours(state: char, x: usize, y: usize) -> u32 {
+    0
+}
+
 
 fn part_1(input: &Map) -> u32 {
     0
