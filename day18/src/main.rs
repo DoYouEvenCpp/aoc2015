914bcd62bfcbec8da637d375e25fb3ee3ceaad6d
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-11 08:35:10 +0100

    Day18
    Some changes in code

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -14,15 +14,6 @@ fn pase_input(input: &str) -> Map {
     data
 }
 
-// fn print_map(map: &Map) {
-//     for x in 0..100 {
-//         for y in 0..100 {
-//             print!("{} ", map[x][y]);
-//         }
-//         println!();
-//     }
-// }
-
 fn does_exist(x: i32, y: i32) -> bool {
     x >= 0 && x < 100 && y >= 0 && y < 100
 }
@@ -36,16 +27,22 @@ fn get_number_of_neighbours(x: usize, y: usize, map: &Map) -> u32 {
         (0, 1),
         (1, -1),
         (1, 0),
-        (1, 1)
-    ].iter()
+        (1, 1),
+    ]
+    .iter()
     .filter(|offset| does_exist(x as i32 + offset.0, y as i32 + offset.1))
     .filter(|offset| map[(x as i32 + offset.0) as usize][(y as i32 + offset.1) as usize] == '#')
     .count() as u32
 }
 
+fn get_number_of_lit_ligts(map: &Map) -> u32 {
+    map.into_iter()
+        .flatten()
+        .filter(|ch| **ch == '#')
+        .fold(0, |sum, _| sum + 1)
+}
 
 fn part_1(input: &Map) -> u32 {
-    let mut count: u32 = 0;
     let mut input = input.to_owned();
     let mut tmp = input.clone();
     for _ in 0..100 {
@@ -63,31 +60,23 @@ fn part_1(input: &Map) -> u32 {
                             tmp[x][y] = '#';
                         }
                     }
-                    _ => panic!("xD")
+                    _ => panic!("xD"),
                 }
             }
         }
         input = tmp.clone();
     }
-    for x in 0..100 {
-        for y in 0..100 {
-            if input[x][y] == '#' {
-                count += 1;
-            }
-        }
-    }
-    count
+    get_number_of_lit_ligts(&input)
 }
 
 fn part_2(input: &Map) -> u32 {
-    let mut count: u32 = 0;
     let mut input = input.to_owned();
     let mut tmp = input.clone();
     for _ in 0..100 {
         input[0][0] = '#';
         input[99][0] = '#';
         input[0][99] = '#';
-        input[99][99] = '#';        
+        input[99][99] = '#';
         for x in 0..100 {
             for y in 0..100 {
                 let number_of_neighbours = get_number_of_neighbours(x, y, &input);
@@ -102,24 +91,17 @@ fn part_2(input: &Map) -> u32 {
                             tmp[x][y] = '#';
                         }
                     }
-                    _ => panic!("xD")
+                    _ => panic!("xD"),
                 }
-                tmp[0][0] = '#';
-                tmp[99][0] = '#';
-                tmp[0][99] = '#';
-                tmp[99][99] = '#';  
             }
         }
+        tmp[0][0] = '#';
+        tmp[99][0] = '#';
+        tmp[0][99] = '#';
+        tmp[99][99] = '#';
         input = tmp.clone();
     }
-    for x in 0..100 {
-        for y in 0..100 {
-            if input[x][y] == '#' {
-                count += 1;
-            }
-        }
-    }
-    count
+    get_number_of_lit_ligts(&input)
 }
 
 fn main() {
@@ -127,9 +109,4 @@ fn main() {
     let content = pase_input(content.trim());
     println!("First puzzle: {}", part_1(&content));
     println!("Second puzzle: {}", part_2(&content));
-}
-
-#[cfg(test)]
-mod day18 {
-    use super::*;
 }
