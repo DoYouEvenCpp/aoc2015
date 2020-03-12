3f2dbc8389172a8dd91947850efdf2e27dd869d3
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-12 14:23:27 +0100

    Day20
    Added method for extracting divisors of a positive integer number

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,3 +1,8 @@
+
+fn get_divisors(number: u32) -> Vec<u32> {
+    (1..=number).filter(|v| number % v == 0).collect()
+}
+
 fn part_1(limit: u32) -> u32 {
     0
 }
@@ -7,7 +12,7 @@ fn part_2(limit: u32) -> u32 {
 }
 
 fn main() {
-    let input = 33100000;
+    let input = 33_100_000;
     println!("First puzzle: {}", part_1(input));
     println!("Second puzzle: {}", part_2(input));
 }
@@ -17,11 +22,16 @@ fn main() {
 mod day20 {
     use super::*;
     #[test]
-    fn test_part_1() {
-    }
-
-    #[test]
-    fn test_part_2() {
+    fn test_get_divisors() {
+        assert_eq!(vec![1], get_divisors(1));
+        assert_eq!(vec![1,2], get_divisors(2));
+        assert_eq!(vec![1,3], get_divisors(3));
+        assert_eq!(vec![1,2,4], get_divisors(4));
+        assert_eq!(vec![1,5], get_divisors(5));
+        assert_eq!(vec![1,2,3,6], get_divisors(6));
+        assert_eq!(vec![1,7], get_divisors(7));
+        assert_eq!(vec![1,2,4,8], get_divisors(8));
+        assert_eq!(vec![1,3,9], get_divisors(9));
     }
 }
 
