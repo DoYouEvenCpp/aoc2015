c12b6b9b94c419e9bfba4e080c048169e8c755a4
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-12 14:13:30 +0100

    Day20 init

---
+++ day20/src/main.rs
@@ -0,0 +1,27 @@
+fn part_1(limit: u32) -> u32 {
+    0
+}
+
+fn part_2(limit: u32) -> u32 {
+    0
+}
+
+fn main() {
+    let input = 33100000;
+    println!("First puzzle: {}", part_1(input));
+    println!("Second puzzle: {}", part_2(input));
+}
+
+
+#[cfg(test)]
+mod day20 {
+    use super::*;
+    #[test]
+    fn test_part_1() {
+    }
+
+    #[test]
+    fn test_part_2() {
+    }
+}
+
