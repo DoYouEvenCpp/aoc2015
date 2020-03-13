fd8d185fccacf5cacc2a2bbb3e1019f38b8e8f25
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-03-13 13:34:11 +0100

    Day20 removal of outdated tests

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -24,41 +24,11 @@ fn part_1(limit: u32) -> u32 {
 }
 
 fn part_2(limit: u32) -> u32 {
-    0
+0
 }
 
 fn main() {
     let input = 33_100_000;
-    println!("First puzzle: {}", part_1(input));
+    //println!("First puzzle: {}", part_1(input));
     println!("Second puzzle: {}", part_2(input));
-}
-
-#[cfg(test)]
-mod day20 {
-    use super::*;
-    #[test]
-    fn test_get_divisors() {
-        assert_eq!(vec![1], get_divisors(1));
-        assert_eq!(vec![1, 2], get_divisors(2));
-        assert_eq!(vec![1, 3], get_divisors(3));
-        assert_eq!(vec![1, 2, 4], get_divisors(4));
-        assert_eq!(vec![1, 5], get_divisors(5));
-        assert_eq!(vec![1, 2, 3, 6], get_divisors(6));
-        assert_eq!(vec![1, 7], get_divisors(7));
-        assert_eq!(vec![1, 2, 4, 8], get_divisors(8));
-        assert_eq!(vec![1, 3, 9], get_divisors(9));
-    }
-
-    #[test]
-    fn test_get_present_count_for_house() {
-        assert_eq!(10, get_present_count_for_house(1));
-        assert_eq!(30, get_present_count_for_house(2));
-        assert_eq!(40, get_present_count_for_house(3));
-        assert_eq!(70, get_present_count_for_house(4));
-        assert_eq!(60, get_present_count_for_house(5));
-        assert_eq!(120, get_present_count_for_house(6));
-        assert_eq!(80, get_present_count_for_house(7));
-        assert_eq!(150, get_present_count_for_house(8));
-        assert_eq!(130, get_present_count_for_house(9));
-    }
 }
